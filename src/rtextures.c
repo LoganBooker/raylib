3fffd856d5d56cf16d5c40e771ca04a046d91b39
Logan <loganbooker@gmail.com>
2024-02-06 22:16:40 +1100

    Whoops

--- src/rtextures.c
+++ src/rtextures.c
@@ -4017,7 +4017,7 @@ void EndUnsafeTextureUpdate(Texture2D texture)
 
 void* BeginUnsafeBufferedTextureUpdate(Texture2D texture, PixelBufferObject readPbo, PixelBufferObject writePbo)
 {
-    return rlBeginUnsafeBufferedPboTextureUpdate(texture.id, 0, 0, texture.width, texture.height, texture.format, readPbo.id, writePbo.od, readPbo.size);
+    return rlBeginUnsafeBufferedPboTextureUpdate(texture.id, 0, 0, texture.width, texture.height, texture.format, readPbo.id, writePbo.id, readPbo.size);
 }
 
 void EndUnsafeBufferedTextureUpdate()
